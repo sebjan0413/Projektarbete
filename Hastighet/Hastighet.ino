2d1c2a319998fb55f86ebdaefe96c5ba0395edb6
sebjan0413 <123723397+sebjan0413@users.noreply.github.com>
2023-04-17 12:14:54 +0200

    Ny  kod för hastighet
    Skrev om koden från grunden

--- Hastighet/Hastighet.ino
+++ Hastighet/Hastighet.ino
@@ -1,22 +1,21 @@
-/********************************************************************************  
-* Arduinokod för framsensorn på självkördande bil
+/********************************************************************************
+* Hastighet.ino: Arduinokod för att justera hastigheten på en självkörande bils 
+*                motor. En signal från en sensor läses in och inverteras.
+*                sensorvärdet styr bilens hastighet. Om sensorvärdet går under
+*                ett bestämt gränsvärde ska backen läggas i.
 ********************************************************************************/
 
 #define enA 9
 #define in1 6
 #define in2 7
-#define button 4
-
-int rotDirection = 0;
-int pressed = false;
 
 void setup() 
 {
+  Serial.begin(9600);
   pinMode(enA, OUTPUT);
   pinMode(in1, OUTPUT);
   pinMode(in2, OUTPUT);
-  pinMode(button, INPUT);
-  
+
   // Set initial rotation direction
   digitalWrite(in1, LOW);
   digitalWrite(in2, HIGH);
@@ -25,32 +24,31 @@ void setup()
 void loop() 
 {
   int potValue = analogRead(A0); // Read potentiometer value
-  int pwmOutput = map(potValue, 0, 1023, 0 , 255); // Map the potentiometer value from 0 to 255
-  analogWrite(enA, pwmOutput); // Send PWM signal to L298N Enable pin
+  int pwmOutput = map(potValue, 0, 800, 255 , 0); // Map the potentiometer value from 0 to 255
 
-  // Read button - Debounce
-  if (digitalRead(button) == true) 
+  if (pwmOutput <= 80) // Om den inverterade signalen är för låg, betyder det att något är nära
   {
-    pressed = !pressed;
+    digitalWrite(in1, HIGH);    // Slå i backen
+    digitalWrite(in2, LOW);     // Slå i backen
+    analogWrite(enA, 100);      // Backa med en hastighet mellan 0-255 (100)
+
+    while (pwmOutput <= 40 && pwmOutput >= 100)
+    
+    delay(1000);      // Delay för smidiagre backning (Mindre ryckigt)
   }
+  else // Annars, om värdet är tillräckligt högt så gasa
+  {
+  digitalWrite(in1, LOW);       // Slår i gasen
+  digitalWrite(in2, HIGH);      // Slår i gasen
+  analogWrite(enA, pwmOutput);  // Gasar med en hastighet mellan 0-255 (pwmOutput)
+  delay(1000);
+  }
+
+  // Skriver ut värdet på den inverterade pwm signalen i konsollen
   
-  while (digitalRead(button) == true);
-  delay(20);
-
-  // If button is pressed - change rotation direction
-  if (pressed == true  & rotDirection == 0) 
-  {
-    digitalWrite(in1, HIGH);
-    digitalWrite(in2, LOW);
-    rotDirection = 1;
-    delay(20);
-  }
-  // If button is pressed - change rotation direction
-  if (pressed == false & rotDirection == 1) 
-  {
-    digitalWrite(in1, LOW);
-    digitalWrite(in2, HIGH);
-    rotDirection = 0;
-    delay(20);
-  }
+  Serial.print("PWM_output:");
+  Serial.print("\t");
+  Serial.print(pwmOutput);
+  Serial.print("\n");
+  delay(250);
 }
